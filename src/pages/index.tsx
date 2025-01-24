8801b23de28bb9eda3d3eef50d58c8e32b69fadc
anotherdev <joseluismanco37@gmail.com>
2025-01-23 21:01:05 -0500

    fix: bad override on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,28 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { AppRoutes } from '@/config/routes'
+import local from '@/services/local-storage/local'
+import { addedSafesSlice, type AddedSafesState } from '@/store/addedSafesSlice'
 
-import Badges from '@/components/badges'
+const IndexPage: NextPage = () => {
+  const router = useRouter()
+  const { chain } = router.query
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Super Account - Badges</title>
-      </Head>
+  useEffect(() => {
+    if (!router.isReady || router.pathname !== AppRoutes.index) {
+      return
+    }
+    const addedSafes = local.getItem<AddedSafesState>(addedSafesSlice.name)
+    const pathname = AppRoutes.welcome.index
 
-      <main>
-        <Badges />
-      </main>
-    </>
-  )
+    router.replace({
+      pathname,
+      query: chain ? { chain } : undefined,
+    })
+  }, [router, chain])
+
+  return <></>
 }
 
-export default Home
+export default IndexPage
