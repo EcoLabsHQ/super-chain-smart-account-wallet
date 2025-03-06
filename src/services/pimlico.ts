c2bcd49e30fcd200af8aade3cdf159e83b0091d5
sebaxor <sebasor@gmail.com>
2025-03-05 22:41:09 -0500

    feat(onchain-identifier): internal and main transaction

--- src/services/pimlico.ts
+++ src/services/pimlico.ts
@@ -6,9 +6,51 @@ import { createPimlicoClient } from 'permissionless/clients/pimlico'
 import { type Address, createPublicClient, http, WalletClient, Transport, Account, Chain } from 'viem'
 import { entryPoint07Address } from 'viem/account-abstraction'
 import { sepolia, optimism } from 'viem/chains'
+import { prepareUserOperation as viemPrepareUserOperation } from "viem/account-abstraction"
+import { ethers } from 'ethers'
+
+
+
+const IDENTIFIER = '5afe003433613232343763663835306565386462343564646561393063346135'
+
+function modifyPaymasterData(request: any, appendString: string): any {
+  if (request.method === "eth_sendUserOperation" && Array.isArray(request.params)) {
+    const modifiedRequest = { ...request };
+    modifiedRequest.params = request.params.map((param: any) => {
+      if (typeof param === "object" && param !== null && "signature" in param) {
+        return {
+          ...param,
+          signature: `${param.signature}${appendString}`
+        };
+      }
+      return param;
+    });
+
+    return modifiedRequest;
+  }
+  return request;
+}
+
+
 
 const pimlicoTransport = () => {
   return http(`${BACKEND_BASE_URI}/user-op-reverse-proxy`, {
+    onFetchRequest(request, init) {
+      if (init?.body) {
+        try {
+
+          let requestBody = JSON.parse(init.body as string);
+
+          requestBody = modifyPaymasterData(requestBody, IDENTIFIER)
+          init.body = JSON.stringify(requestBody);
+
+          console.log("🔹 Request modificado:", requestBody);
+        } catch (error) {
+          console.error("❌ Error modificando el request body:", error);
+        }
+      }
+
+    },
     fetchOptions: {
       credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
@@ -20,6 +62,7 @@ export const publicClient = createPublicClient({
   transport: http(JSON_RPC_PROVIDER),
   chain: optimism,
 })
+
 
 export const paymasterClient = () =>
   createPimlicoClient({
@@ -60,16 +103,30 @@ export async function getSmartAccountClient(
     paymaster: paymasterClient(),
     userOperation: {
       estimateFeesPerGas: async () => (await paymasterClient().getUserOperationGasPrice()).fast,
+      prepareUserOperation: async (client, parameters_) => {
+
+        console.log("Identifier op", parameters_);
+        parameters_.calls!.forEach((element: any) => {
+
+          const encodedData = ethers.solidityPacked(
+            ['bytes', 'bytes'],
+            [element.data, '0x' + IDENTIFIER]
+          );
+          element.data = encodedData
+          console.log("Identifier op", element);
+        });
+
+        return await viemPrepareUserOperation(client, parameters_)
+      }
     },
-    // middleware: {
-    //   sponsorUserOperation: async (args: any) => {
-    //     return paymasterClient().sponsorUserOperation({
-    //       ...args,
-    //       sponsorshipPolicyId: 'sp_burly_overlord',
-    //     })
-    //   },
-    //   gasPrice: async () => (await pimlicoBundlerClient().getUserOperationGasPrice()).fast, // if using pimlico bundler
-    // },
   })
+
+  paymasterClient().getPaymasterData
+  const originalSendUserOperation = smartAccountClient.sendUserOperation;
+  smartAccountClient.sendUserOperation = async (userOp) => {
+    console.log('OEEEEEEEEEEEEEEEEEEEEEEEEEE');
+    return originalSendUserOperation(userOp);
+  };
+
   return smartAccountClient
 }
