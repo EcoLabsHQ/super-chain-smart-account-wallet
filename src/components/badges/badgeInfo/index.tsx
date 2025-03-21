a2d54b45172747ec47ca0ff11bdd901b5f514cab
sebaxor <sebasor@gmail.com>
2025-03-21 04:10:49 -0500

    feat(badges): visual adjustments

--- src/components/badges/badgeInfo/index.tsx
+++ src/components/badges/badgeInfo/index.tsx
@@ -11,6 +11,7 @@ import SuperChainPoints from '@/public/images/common/superChain.svg'
 import { Chip } from '@/components/common/Chip'
 import Image from 'next/image'
 import CheckCircleIcon from '@/public/images/common/check-circle.svg'
+import NetworkChip from '../networkChip'
 
 function BadgeInfo({
   currentBadge,
@@ -269,42 +270,6 @@ function BadgeInfo({
         </CardContent>
       </Card>
     </Stack>
-  )
-}
-const networks = {
-  Optimism: 'https://safe-transaction-assets.safe.global/chains/10/chain_logo.png',
-  Base: 'https://safe-transaction-assets.safe.global/chains/8453/chain_logo.png',
-  Mode: 'https://account.superchain.eco/chains/34443/chain_logo.svg',
-  Ethereum: 'https://safe-transaction-assets.safe.global/chains/1/chain_logo.png',
-  Lisk: 'https://account.superchain.eco/chains/1135/chain_logo.svg',
-} as const
-
-interface NetworkChipProps {
-  network: string
-}
-const NetworkChip: React.FC<NetworkChipProps> = ({ network }) => {
-  const networkLogo = networks[network as keyof typeof networks]
-
-  return (
-    <Chip
-      label={
-        <Box display="flex" gap={1} alignItems="center">
-          <Image src={networkLogo} alt={`${network} Logo`} width={24} height={24} loading="lazy" />
-          <strong>{network}</strong>
-        </Box>
-      }
-      sx={{
-        borderRadius: '18px',
-        color: 'black',
-        border: '1px solid #E1E2EA',
-        backgroundColor: 'transparent',
-        height: '32px',
-        fontWeight: 600,
-        minWidth: 'auto',
-        alignSelf: 'flex-start',
-        maxWidth: 'fit-content',
-      }}
-    />
   )
 }
 
