5c9ac04f5c4cc583e6c73c1a0154ebd7c2ac7442
sebaxor <sebasor@gmail.com>
2025-04-03 22:58:52 -0500

    feat(season 7): link on chip

--- src/components/badges/seasonChip/index.tsx
+++ src/components/badges/seasonChip/index.tsx
@@ -2,45 +2,52 @@ import { Box, Typography } from '@mui/material'
 import React from 'react'
 import { Chip } from '@/components/common/Chip'
 import Image from 'next/image'
+import Link from 'next/link'
+import { AppRoutes } from '@/config/routes'
+import { useRouter } from 'next/router'
 
 function SeasonChip({ season, style }: { season: number; style: 'info' | 'badge' }) {
   const isBadge = style === 'badge'
-
+  const link = AppRoutes.badges.season7
+  const router = useRouter()
+  const query = router.query.safe ? { safe: router.query.safe } : undefined
   return (
-    <Chip
-      label={
-        <Box display="flex" alignItems="center" gap={0.5}>
-          <Typography
-            fontWeight={600}
-            sx={{
-              color: '#6B5DE7',
-              margin: '-5px',
-            }}
-          >
-            S{season}
-          </Typography>
-          <Image
-            src="/images/badges/stars_custom.svg"
-            alt="Star Icon"
-            width={14}
-            height={14}
-            style={{ margin: '2px' }}
-          />
-        </Box>
-      }
-      sx={{
-        borderRadius: '18px',
-        border: '1px solid #6B5DE7',
-        backgroundColor: '#F4F0FF',
-        padding: '6px 10px',
-        height: '32px',
-        maxWidth: '60px',
-        fontWeight: 600,
-        transform: isBadge ? 'scale(0.8)' : 'scale(1)',
-        transformOrigin: 'top left',
-        visibility: !season ? 'hidden' : 'visible',
-      }}
-    />
+    <Link onClick={(e) => e.stopPropagation()} href={{ pathname: link, query }} passHref>
+      <Chip
+        label={
+          <Box display="flex" alignItems="center" gap={0.5}>
+            <Typography
+              fontWeight={600}
+              sx={{
+                color: '#6B5DE7',
+                margin: '-5px',
+              }}
+            >
+              S{season}
+            </Typography>
+            <Image
+              src="/images/badges/stars_custom.svg"
+              alt="Star Icon"
+              width={14}
+              height={14}
+              style={{ margin: '2px' }}
+            />
+          </Box>
+        }
+        sx={{
+          borderRadius: '18px',
+          border: '1px solid #6B5DE7',
+          backgroundColor: '#F4F0FF',
+          padding: '6px 10px',
+          height: '32px',
+          maxWidth: '60px',
+          fontWeight: 600,
+          transform: isBadge ? 'scale(0.8)' : 'scale(1)',
+          transformOrigin: 'top left',
+          visibility: !season ? 'hidden' : 'visible',
+        }}
+      />
+    </Link>
   )
 }
 
