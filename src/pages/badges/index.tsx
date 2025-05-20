f2e3d6eba961f1b4514e59d1b85d748e7b744441
sebaxor <sebasor@gmail.com>
2025-05-20 02:37:29 -0500

    captcha

--- src/pages/badges/index.tsx
+++ src/pages/badges/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-
+import Turnstile from 'react-cloudflare-turnstile'
 import Badges from '@/components/badges'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+  const [token, setToken] = useState<string | null>(null)
+  const handleToken = (token: string) => {
+    setToken(token)
+    console.log('Captcha token:', token)
+  }
   return (
     <>
       <Head>
@@ -12,6 +18,7 @@ const Home: NextPage = () => {
 
       <main>
         <Badges />
+        <Turnstile callback={handleToken} turnstileSiteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY!} />
       </main>
     </>
   )
