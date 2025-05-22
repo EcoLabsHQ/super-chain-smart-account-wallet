109b387c3b30bcae20fc3ad3c01eacdf696eb40a
sebaxor <49530370+sebaxor@users.noreply.github.com>
2025-05-22 14:19:10 -0500

    feat(links): help and feedback links

--- src/components/sidebar/SidebarFooter/index.tsx
+++ src/components/sidebar/SidebarFooter/index.tsx
@@ -38,12 +38,7 @@ const SidebarFooter = (): ReactElement => {
       )} */}
 
       <ListItem disablePadding>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://tally.superchain.eco/accountsform"
-          style={{ width: '100%' }}
-        >
+        <a target="_blank" rel="noopener noreferrer" href="https://t.me/superchaineco" style={{ width: '100%' }}>
           <SidebarListItemButton>
             <SidebarListItemIcon color="primary">
               <FeedbackIcon />
@@ -55,12 +50,7 @@ const SidebarFooter = (): ReactElement => {
         </a>
       </ListItem>
       <ListItem disablePadding>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://ecosystem-accounts.super.site/"
-          style={{ width: '100%' }}
-        >
+        <a target="_blank" rel="noopener noreferrer" href=" https://docs.superchain.eco/" style={{ width: '100%' }}>
           <SidebarListItemButton>
             <SidebarListItemIcon color="primary">
               <DocsIcon />
