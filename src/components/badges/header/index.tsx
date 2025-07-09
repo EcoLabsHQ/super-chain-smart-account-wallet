a85d56f50f42a452f284f8facd6cf306ef06c390
sebaxor <49530370+sebaxor@users.noreply.github.com>
2025-07-08 22:20:28 -0500

    feat: restore Season 8 badge navigation item

--- src/components/badges/header/index.tsx
+++ src/components/badges/header/index.tsx
@@ -15,10 +15,10 @@ export const badgesNavItems = [
     label: 'Season 7',
     href: AppRoutes.badges.season7,
   },
-  // {
-  //   label: 'Season 8',
-  //   href: AppRoutes.badges.season8,
-  // },
+  {
+    label: 'Season 8',
+    href: AppRoutes.badges.season8,
+  },
 ]
 
 function BadgesHeader({
